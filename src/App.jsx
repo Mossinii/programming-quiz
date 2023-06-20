8532ff01cd29f34c337c409e8adaecd34b149764
Mossinii <117039278+Mossinii@users.noreply.github.com>
2023-06-20 02:13:15 -0300

    feat: last commit, project ok!

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
-import { useState } from 'react'
+import { useContext, useEffect } from "react";
+import { QuizContext } from "./context/quiz";
 
-import './App.css'
-import { Welcome } from './components/Welcome'
+import Welcome from "./components/Welcome";
+import Question from "./components/Question";
+import GameOver from "./components/GameOver";
+
+import PickCategory from "./components/PickCategory";
+
+import "./App.css";
 
 function App() {
+  const [quizState, dispatch] = useContext(QuizContext);
 
   return (
-       <div className='App'>
-     <h1>Quiz de Programação</h1>   
-     <Welcome/>
-      </div>
-  )
+    <div className="App">
+      <h1>Quiz de Programação</h1>
+      {quizState.gameStage === "Start" && <Welcome />}
+      {quizState.gameStage === "Category" && <PickCategory />}
+      {quizState.gameStage === "Playing" && <Question />}
+      {quizState.gameStage === "End" && <GameOver />}
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/context/quiz.jsx
+++
@@ -1,9 +0,0 @@
- import { createContext } from "react";
-
- export const QuizContext = createContext();
-
- export const QuizProvider = ({chilren})=> {
-const value= {};
-
-return <QuizContext.Provider value={value}> { chilren } </QuizContext.Provider>;
-     }
